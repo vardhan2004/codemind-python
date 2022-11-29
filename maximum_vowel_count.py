35cf42b57a32514a22d0e1fdc757aebaaf1f5734
vardhan2004 <117373001+vardhan2004@users.noreply.github.com>
2022-11-29 22:15:08 +0530

    def vow_count(n):     a='aeiouAEIOU'     cnt=0     for i in n:         if i in a:             cnt+=1     return cnt
    s=input()
    #print(s)
    d=s.split()
    b=[]
    for i in d:
        c=vow_count(i)
        b.append(c)
    print(max(b))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,16 @@
+def vow_count(n):
+    a='aeiouAEIOU'
+    cnt=0
+    for i in n:
+        if i in a:
+            cnt+=1
+    return cnt
+        
+s=input()
+#print(s)
+d=s.split()
+b=[]
+for i in d:
+    c=vow_count(i)
+    b.append(c)
+print(max(b))
