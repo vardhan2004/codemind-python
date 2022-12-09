95a9c34bb8f0361488bca40e48b25bc99383e4f7
vardhan2004 <117373001+vardhan2004@users.noreply.github.com>
2022-12-09 23:01:04 +0530

    s=input() s=s.lower() a=[] for i in s:     if i not in a and i.isalpha():         a.append(i) a.sort() s=''.join(a) print(s)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,9 @@
 s=input()
 s=s.lower()
-#print(s)
 a=[]
 for i in s:
-   if s.count(i)==1 and i.isalpha():
-       a.append(i)
+    if i not in a and i.isalpha():
+        a.append(i)
 a.sort()
 s=''.join(a)
 print(s)
