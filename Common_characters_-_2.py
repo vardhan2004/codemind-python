d67807488ad703d72f842ed0310a06b70d8403d8
vardhan2004 <117373001+vardhan2004@users.noreply.github.com>
2022-12-10 17:13:53 +0530

    def fun(key,ar):     j=1     cnt=0     while j<len(ar):         if key in ar[j]:             cnt+=1         j+=1     return cnt s=input() s=s.lower() a=s.split() k=a[0] j=1 cnt=0 c=[] for i in k:     res=fun(i,a)     if res==len(a)-1:         cnt+=1         c.append(i) print(len(c))

---
+++ Common_characters_-_2.py
@@ -0,0 +1,22 @@
+def fun(key,ar):
+    j=1
+    cnt=0
+    while j<len(ar):
+        if key in ar[j]:
+            cnt+=1
+        j+=1
+    return cnt
+s=input()
+s=s.lower()
+a=s.split()
+k=a[0]
+j=1
+cnt=0
+c=[]
+for i in k:
+    res=fun(i,a)
+    if res==len(a)-1:
+        cnt+=1
+        c.append(i)
+print(len(c))
+    
