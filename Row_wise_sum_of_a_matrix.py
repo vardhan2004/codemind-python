4044008dac505c7868e21e72ad9f709a93b2d18f
vardhan2004 <117373001+vardhan2004@users.noreply.github.com>
2022-12-15 18:30:47 +0530

    def row_sum(d,r,c):     r_s=[0 for i in range(r)]     for i in range(r):         for j in range(c):             r_s[i]+=d[i][j]     return r_s
    r,c=map(int,input().split())
    row=[0 for i in range(c)]
    d=[row.copy() for i in range(r)]
    for i in range(r):
        l=list(map(int,input().split()))
        for j in range(c):
            d[i][j]=l[j]
               
    res=row_sum(d,r,c)
    print(*res)

---
+++ Row_wise_sum_of_a_matrix.py
@@ -0,0 +1,17 @@
+def row_sum(d,r,c):
+    r_s=[0 for i in range(r)]
+    for i in range(r):
+        for j in range(c):
+            r_s[i]+=d[i][j]
+    return r_s
+
+r,c=map(int,input().split())
+row=[0 for i in range(c)]
+d=[row.copy() for i in range(r)]
+for i in range(r):
+    l=list(map(int,input().split()))
+    for j in range(c):
+        d[i][j]=l[j]
+           
+res=row_sum(d,r,c)
+print(*res)
