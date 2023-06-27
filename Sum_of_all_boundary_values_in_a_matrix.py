210e6cc2d61b8ca4aa4656254674be5e39572f75
vardhan2004 <117373001+vardhan2004@users.noreply.github.com>
2023-06-27 22:03:13 +0530

    n, m = map(int, input().split()) r = [0 for i in range(m)] M = [r.copy() for i in range(n)] for i in range(n):     l = list(map(int,input().split()))     for j in range(m):         M[i][j] = l[j] k = [0, n - 1] f = [0, m - 1] ubs = 0 for i in range(n):     for j in range(m):         if i in k or j in f:             ubs += M[i][j] print(ubs)

---
+++ Sum_of_all_boundary_values_in_a_matrix.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+r = [0 for i in range(m)]
+M = [r.copy() for i in range(n)]
+for i in range(n):
+    l = list(map(int,input().split()))
+    for j in range(m):
+        M[i][j] = l[j]
+k = [0, n - 1]
+f = [0, m - 1]
+ubs = 0
+for i in range(n):
+    for j in range(m):
+        if i in k or j in f:
+            ubs += M[i][j]
+print(ubs)
